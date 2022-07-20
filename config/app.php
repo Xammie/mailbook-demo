1c6a5fb849b0bcd7e2078fcbb2b0d58b729d5852
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-07-20 17:58:59 +0200

    Cleanup

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Mailbook'),
 
     /*
     |--------------------------------------------------------------------------
@@ -190,11 +190,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
