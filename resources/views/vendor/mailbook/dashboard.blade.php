77891b3259fb2f2cb75163111e13bc3f477b369d
Max Hoogenbosch <mhoogenbosch@enrise.com>
2026-08-01 14:39:10 +0200

    Update to mailbook 1.12.0

--- resources/views/vendor/mailbook/dashboard.blade.php
+++ resources/views/vendor/mailbook/dashboard.blade.php
@@ -11,28 +11,30 @@
     </script>
     <meta name="description" content="Mailbook is a Laravel package that lets you easily inspect your mails without having to actually trigger it in your application.">
     <link rel="canonical" href="{{ route('mailbook.dashboard') }}" />
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ $subject }} - Mailbook</title>
-    <style>{{ $style }}</style>
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✉️</text></svg>">
+    <style>
+        {{ $style }}
+    </style>
+    <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✉️</text></svg>"
+    />
 </head>
-<body class="bg-gray-900 text-white h-screen w-screen flex flex-col">
+<body class="flex h-screen w-screen flex-col bg-gray-900 text-white">
 @include('mailbook::navigation')
-<div class="flex grow shrink items-stretch overflow-hidden">
+<div class="flex shrink grow items-stretch overflow-hidden">
     @include('mailbook::list')
     @include('mailbook::display')
     @include('mailbook::details')
 </div>
 <script>
-    const select = document.getElementById('locale');
-    select.addEventListener('change', (event) => {
-        const queryVariables = new URLSearchParams(window.location.search);
-        queryVariables.set('locale', event.target.value);
-        window.location.search = queryVariables.toString();
-    });
+    {{ $script }}
 </script>
 </body>
 </html>
-
